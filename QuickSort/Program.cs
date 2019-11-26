286e02140411a7ca0a83f6e168f569fcc6972648
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2019-11-26 16:44:39 +0800

    quicksort comments

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -12,6 +12,12 @@ namespace QuickSort
             Console.ReadLine();
         }
 
+        //1.两个方向，左边的i下标一直往右走，当a[i] <= a[center_index]，其中center_index是中枢元素的数组下标，一般取为数组第0个元素。而右边的j下标一直往左走，当a[j] > a[center_index]
+        //2.如果i和j都走不动了，i <= j, 交换a[i] 和a[j], 重复上面的过程，直到i>j
+        //3.交换a[j] 和a[center_index]，完成一趟快速排序
+        //4.在中枢元素和a[j] 交换的时候，很有可能把前面的元素的稳定性打乱，比如序列为 5 3 3 4 3 8 9 10 11， 现在中枢元素5和3(第5个元素，下标从1开始计)交换就会把元素3的稳定性打乱
+        //5.不稳定发生在中枢元素和a[j] 交换的时刻
+        //6.不稳定的排序算法。
         static void QuickSort(int[] a,int left,int right)
         {
             if (left >= right) return;
