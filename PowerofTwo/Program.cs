e71ddacbee9d9dd1b6aed6b74e286e481d3e887c
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2020-03-08 16:18:28 +0800

    PowerofTwo

---
+++ PowerofTwo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PowerofTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPowerofTwo(1);
+            IsPowerofTwo(2);
+            IsPowerofTwo(3);
+            IsPowerofTwo(4);
+            IsPowerofTwo(15);
+            IsPowerofTwo(65536);
+            Console.Read();
+        }
+
+        static bool IsPowerofTwo(int n)
+        {
+            if (n < 1) return false;
+            bool result = (n & (n - 1)) == 0;
+            if (result)
+            {
+                Console.WriteLine($"{n}是2的幂次方");
+            }
+            else
+            {
+                Console.WriteLine($"{n}不是2的幂次方");
+            }
+            return result;
+        }
+    }
+}
